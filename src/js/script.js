81850fad879d98560543a5a2687eb279bddde00c
pavel.mishinfz <pavel.mishinfz@gmail.com>
2023-03-18 01:15:43 +0300

    Добавлен функционал бургер-меню

--- src/js/script.js
+++ src/js/script.js
@@ -1,43 +1,49 @@
-let center = [55.10776337928515, 38.74434368650815];
-ymaps.ready(init);
+const hamb = document.querySelector('#hamb');
+const menu = document.querySelector('#menu');
+const body = document.body;
+const popup = document.querySelector('#popup');
 
-function init() {
-    let map = new ymaps.Map('map', {
-        center: center,
-        zoom: 17,
-        controls: ['routePanelControl']
-    });
+hamb.addEventListener("click", hambHandler);
 
-    let placemark = new ymaps.Placemark(center, {}, {
-        iconLayout: 'default#image',
-        iconImageHref: 'http://127.0.0.1:5500/src/img/contacts/icon-mark.svg',
-        iconImageSize: [40, 40],
-        iconImageOffset: [-20, -30]
-    });
-
-    map.controls.remove('geolocationControl'); // удаляем геолокацию
-    map.controls.remove('searchControl'); // удаляем поиск
-    map.controls.remove('trafficControl'); // удаляем контроль трафика
-    map.controls.remove('typeSelector'); // удаляем тип
-    map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
-    map.controls.remove('zoomControl'); // удаляем контрол зуммирования
-    map.controls.remove('rulerControl'); // удаляем контрол правил
-    // map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
-
-    let control = map.controls.get('routePanelControl');
-    let location = ymaps.geolocation.get();
-
-    location.then(function (res) {
-        let locationText = res.geoObjects.get(0).properties.get('text');
-        control.routePanel.state.set({
-            type: 'pedestrian',
-            fromEnabled: true,
-            from: locationText,
-            toEnabled: false,
-            to: 'г. Коломна, ул. Октябрьской революции д. 161'
-        });
-    });
-
-
-    map.geoObjects.add(placemark);
+function hambHandler(e) {
+    e.preventDefault();
+    menu.classList.toggle("open");
+    hamb.classList.toggle("active");
+    body.classList.toggle("noscroll");
+    popup.classList.toggle("popup");
 }
+
+const close = document.querySelector('.menu__close');
+
+close.addEventListener("click", closeOnClick);
+
+function closeOnClick(e) {
+    e.preventDefault();
+    menu.classList.remove("open");
+    hamb.classList.remove("active");
+    body.classList.remove("noscroll");
+    popup.classList.remove("popup");
+}
+
+const hambFooter = document.querySelector('#hamb-footer');
+const footerLeft = document.querySelector('.footer__left');
+const footerRight = document.querySelector('.footer__right');
+
+
+hambFooter.addEventListener("click", hambHandlerFooter);
+
+function hambHandlerFooter(e) {
+    e.preventDefault();
+    hambFooter.classList.toggle("active");
+    footerLeft.classList.toggle("hide");
+    footerRight.classList.toggle("show");
+}
+
+document.addEventListener('mouseup', function (e) {
+    let container = document.querySelector(".footer__right");
+    if (!container.contains(e.target)) {
+        hambFooter.classList.remove("active");
+        footerLeft.classList.remove("hide");
+        footerRight.classList.remove("show");
+    }
+});
