0a8d56f5e564a8e9939f3941442ba9bae7fa2045
pavel.mishinfz <pavel.mishinfz@gmail.com>
2023-04-20 20:49:57 +0300

    Formation of modal windows using ajax

--- dental-clinic/public/js/popup.js
+++ dental-clinic/public/js/popup.js
@@ -1,88 +1,125 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!*******************************!*\
-  !*** ./resources/js/popup.js ***!
-  \*******************************/
-/* Интерактив обратной формы связи */
-var buttonsFeedback = document.querySelectorAll('.btn--feedback');
-var formFeedback = document.querySelector('.popup-feedback');
-buttonsFeedback.forEach(function (btn) {
-  return btn.addEventListener("click", formFeedbackHandler);
-});
-function formFeedbackHandler(e) {
-  e.preventDefault();
-  formFeedback.classList.toggle("open");
-  document.body.classList.toggle("noscroll");
-}
-var closeFeedbackForm = document.querySelector('.popup__close--feedback');
-closeFeedbackForm.addEventListener("click", closeOnClickForm);
-function closeOnClickForm(e) {
-  e.preventDefault();
-  formFeedback.classList.remove("open");
-  document.body.classList.remove("noscroll");
-}
+/******/
+(() => { // webpackBootstrap
+    let __webpack_exports__ = {};
+    /*!*******************************!*\
+      !*** ./resources/js/popup.js ***!
+      \*******************************/
+    /* Интерактив обратной формы связи */
+    let buttonsFeedback = document.querySelectorAll('.btn--feedback');
+    let formFeedback = document.querySelector('.popup-feedback');
+    buttonsFeedback.forEach(function (btn) {
+        return btn.addEventListener("click", formFeedbackHandler);
+    });
 
-/* Интерактив электронного помощника */
-var helperItems = document.querySelectorAll('.helpers__list-item');
-var helperPopup = document.querySelector('.popup');
-helperItems.forEach(function (item) {
-  return item.addEventListener("click", helperItemsHandler);
-});
-function helperItemsHandler(e) {
-  e.preventDefault();
-  helperPopup.classList.toggle("open");
-  document.body.classList.toggle("noscroll");
-}
-var closeHelperForm = document.querySelector('.popup__close--helpers');
-if (closeHelperForm) {
-  var closeOnClickHelperForm = function closeOnClickHelperForm(e) {
-    e.preventDefault();
-    helperPopup.classList.remove("open");
-    document.body.classList.remove("noscroll");
-  };
-  closeHelperForm.addEventListener("click", closeOnClickHelperForm);
-}
+    function formFeedbackHandler(e) {
+        e.preventDefault();
+        formFeedback.classList.toggle("open");
+        document.body.classList.toggle("noscroll");
+    }
 
-/* Запись к конкретному врачу */
-var doctorItems = document.querySelectorAll('.item--click');
-var doctorPopup = document.querySelector('.popup--doctor');
-doctorItems.forEach(function (item) {
-  return item.addEventListener("click", doctorItemsHandler);
-});
-function doctorItemsHandler(e) {
-  e.preventDefault();
-  doctorPopup.classList.toggle("open");
-  helperPopup.classList.remove("open");
-}
-var closeDoctorForm = document.querySelector('.popup__close--doctor');
-if (closeDoctorForm) {
-  var closeOnClickDoctorForm = function closeOnClickDoctorForm(e) {
-    e.preventDefault();
-    doctorPopup.classList.remove("open");
-    document.body.classList.remove("noscroll");
-  };
-  closeDoctorForm.addEventListener("click", closeOnClickDoctorForm);
-}
+    let closeFeedbackForm = document.querySelector('.popup__close--feedback');
+    closeFeedbackForm.addEventListener("click", closeOnClickForm);
 
-/* Узнать цены */
-var priceButtons = document.querySelectorAll('.btn--price');
-var pricePopup = document.querySelector('.popup--price-list');
-priceButtons.forEach(function (btn) {
-  return btn.addEventListener("click", pricePopupHandler);
-});
-function pricePopupHandler(e) {
-  e.preventDefault();
-  pricePopup.classList.toggle("open");
-  document.body.classList.toggle("noscroll");
-}
-var closePricePopup = document.querySelector('.popup__close--price-list');
-if (closePricePopup) {
-  var closeOnClickPricePopup = function closeOnClickPricePopup(e) {
-    e.preventDefault();
-    pricePopup.classList.remove("open");
-    document.body.classList.remove("noscroll");
-  };
-  closePricePopup.addEventListener("click", closeOnClickPricePopup);
-}
-/******/ })()
+    function closeOnClickForm(e) {
+        e.preventDefault();
+        formFeedback.classList.remove("open");
+        document.body.classList.remove("noscroll");
+    }
+
+    /* Интерактив электронного помощника */
+    let helpersForms = document.querySelectorAll('.helpers__form');
+    let popupHelpers = document.querySelector('.popup--helpers');
+
+    $(helpersForms).each(function () {
+        $(this).on('submit', function (e) {
+            e.preventDefault();
+
+            let id = $(this).data('id-department');
+
+            $.ajax({
+                url: "/popup/helpers",
+                type: "GET",
+                data: {
+                    id: id,
+                },
+                success: function (response) {
+                    $('.popup--helpers').html(response.tmp);
+
+                    $(popupHelpers).addClass("open");
+                    $(document.body).addClass("noscroll");
+
+                    let closeHelperForm = document.querySelector('.popup__close--helpers');
+                    let closeOnClickHelperForm = function closeOnClickHelperForm(e) {
+                        e.preventDefault();
+                        $(popupHelpers).removeClass("open");
+                        $(document.body).removeClass("noscroll");
+                    };
+                    closeHelperForm.addEventListener("click", closeOnClickHelperForm);
+
+                    /* Запись к конкретному врачу */
+                    let doctorForms = document.querySelectorAll('.popup__form-doctor');
+                    let doctorPopup = document.querySelector('.popup--doctor');
+
+                    $(doctorForms).each(function(){
+                        $(this).on('submit', function (e){
+                            e.preventDefault();
+
+                            id = $(this).data('id-doctor');
+
+                            $.ajax({
+                                url: "/popup/doctor",
+                                type: "GET",
+                                data: {
+                                    id: id,
+                                },
+                                success: function (response) {
+                                    $('.popup--doctor').html(response.tmp);
+
+                                    $(doctorPopup).addClass("open");
+                                    $(popupHelpers).removeClass("open");
+
+                                    let closeDoctorForm = document.querySelector('.popup__close--doctor');
+                                    let closeOnClickDoctorForm = function closeOnClickDoctorForm(e) {
+                                        e.preventDefault();
+                                        $(doctorPopup).removeClass("open");
+                                        $(document.body).removeClass("noscroll");
+                                    };
+                                    closeDoctorForm.addEventListener("click", closeOnClickDoctorForm);
+
+                                    console.log(response.success);
+                                }
+                            });
+                        });
+                    });
+
+                    console.log(response.success);
+                },
+            });
+        });
+    });
+
+    /* Узнать цены */
+    let priceButtons = document.querySelectorAll('.btn--price');
+    let pricePopup = document.querySelector('.popup--price-list');
+    priceButtons.forEach(function (btn) {
+        return btn.addEventListener("click", pricePopupHandler);
+    });
+
+    function pricePopupHandler(e) {
+        e.preventDefault();
+        pricePopup.classList.toggle("open");
+        document.body.classList.toggle("noscroll");
+    }
+
+    let closePricePopup = document.querySelector('.popup__close--price-list');
+    if (closePricePopup) {
+        let closeOnClickPricePopup = function closeOnClickPricePopup(e) {
+            e.preventDefault();
+            pricePopup.classList.remove("open");
+            document.body.classList.remove("noscroll");
+        };
+        closePricePopup.addEventListener("click", closeOnClickPricePopup);
+    }
+    /******/
+})()
 ;
