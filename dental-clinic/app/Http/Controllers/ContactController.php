f357e777d4b05283420be1e1bd0ff4c3de01fa96
pavel.mishinfz <pavel.mishinfz@gmail.com>
2023-04-25 22:03:29 +0300

    Header output

--- dental-clinic/app/Http/Controllers/ContactController.php
+++ dental-clinic/app/Http/Controllers/ContactController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 class ContactController extends Controller
 {
     public function index() {
-        return view('contacts');
+        $title = 'Contacts';
+
+        return view('contacts', compact('title'));
     }
 }
