fbcd4228d672ae154ba2ad3d04fca9817bbf9fb2
pavel.mishinfz <pavel.mishinfz@gmail.com>
2023-05-15 00:20:31 +0300

    Correct operation on the contacts page is configured

--- dental-clinic/public/js/captcha.js
+++ dental-clinic/public/js/captcha.js
@@ -1,11 +1,14 @@
 $(document).on('click', '#b-captcha__reload', function (e) {
     e.preventDefault();
 
+    let captcha_img;
+    $(this).hasClass('b-captcha__reload--free') ? captcha_img = $('#b-captcha__img--free') : captcha_img = $('#b-captcha__img');
+
     $.ajax({
         type: 'GET',
         url: '/reload',
         success: function (response) {
-            $('#b-captcha__img').html(response.captcha);
+            captcha_img.html(response.captcha);
         }
     });
 
