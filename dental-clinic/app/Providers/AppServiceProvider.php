f2c47fc55bb6cf4639457d85cf583308db452dd4
pavel.mishinfz <pavel.mishinfz@gmail.com>
2023-05-24 00:07:29 +0300

    Replacing public with public_html

--- dental-clinic/app/Providers/AppServiceProvider.php
+++ dental-clinic/app/Providers/AppServiceProvider.php
@@ -22,7 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind('path.public', function() {
+            return base_path('public_html');
+        });
     }
 
     /**
