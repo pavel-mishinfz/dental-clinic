7473beec92dff4d4eb5467491ef973b155f69f65
pavel.mishinfz <pavel.mishinfz@gmail.com>
2023-05-23 14:30:43 +0300

    Added auxiliary variables for the administration panel

--- dental-clinic/app/Providers/AppServiceProvider.php
+++ dental-clinic/app/Providers/AppServiceProvider.php
@@ -2,6 +2,15 @@
 
 namespace App\Providers;
 
+use App\Models\Department;
+use App\Models\DepartmentDoctor;
+use App\Models\Doctor;
+use App\Models\FormPatient;
+use App\Models\License;
+use App\Models\Patient;
+use App\Models\Review;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +32,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $departments = Department::all();
+        $doctors = Doctor::all();
+        $department_doctors = DepartmentDoctor::all();
+        $patients = Patient::all();
+        $form_patients = FormPatient::all();
+        $licenses = License::all();
+        $reviews = Review::all();
+
+        Config::set(['admin' => [
+            'department' => $departments->count(),
+            'doctor' => $doctors->count(),
+            'department_doctor' => $department_doctors->count(),
+            'patient' => $patients->count(),
+            'form_patient' => $form_patients->count(),
+            'license' => $licenses->count(),
+            'review' => $reviews->count(),
+        ]]);
+
+        Paginator::useBootstrap();
     }
 }
