d4ff73f002406f19c3dd6deb4db28709e0b5d448
pavel.mishinfz <pavel.mishinfz@gmail.com>
2023-05-23 14:18:42 +0300

    Bootstrap connection

--- dental-clinic/webpack.mix.js
+++ dental-clinic/webpack.mix.js
@@ -6,16 +6,11 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.js('resources/js/hamb-menu.js', 'public/js/hamb-menu.js');
-mix.js('resources/js/observer.js', 'public/js/observer.js');
-mix.js('resources/js/popup.js', 'public/js/popup.js');
-mix.js('resources/js/ymaps.js', 'public/js/ymaps.js');
-
-mix.styles('resources/css/normalize.css', 'public/css/normalize.css');
-mix.styles('resources/css/style.css', 'public/css/style.css');
-mix.styles('resources/css/fonts.css', 'public/css/fonts.css');
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .sourceMaps();
